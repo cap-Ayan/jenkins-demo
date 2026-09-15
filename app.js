b71075c5ed950b8b2aa8e52389465ef0e4d4c9e8
Ayan Panda <aimerss721@gmail.com>
2026-09-15 17:01:04 +0530

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from Jenkins + ECR + EC2!....jfkcvxvxcvcxvcxvcxvxcg......mewwwfdfdsffdf");
+    res.end("Version 2 - Blue Green!");
 });
 
 server.listen(PORT, () => {
